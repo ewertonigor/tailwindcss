c79033bd61dbcc12063dadf52a055acbf4b1ffec
Ewerton Igor <ewertonigor@icloud.com>
2024-04-13 20:50:54 -0300

    Widget de espaço usado - SideBar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,20 @@
-import { Blend, Search } from 'lucide-react'
-import MainNavigation from './MainNavigation'
+import {
+  BarChart,
+  Blend,
+  CheckSquare,
+  Cog,
+  Flag,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+import NavItem from './NavItem'
+import UsedSpaceWidget from './UsedSpaceWidget'
 
 export default function SideBar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <strong className="mx-1 flex items-center gap-2 text-xl font-semibold text-zinc-900">
         <Blend />
         <span>Untitled UI</span>
@@ -15,7 +26,23 @@ export default function SideBar() {
           placeholder="Search"
         />
       </div>
-      <MainNavigation />
+      <nav className="space-y-0.5">
+        <NavItem icon={Blend} title="Home" />
+        <NavItem icon={BarChart} title="Dashboard" />
+        <NavItem icon={SquareStack} title="Projects" />
+        <NavItem icon={CheckSquare} title="Tasks" />
+        <NavItem icon={Flag} title="Reporting" />
+        <NavItem icon={Users} title="Users" />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem icon={LifeBuoy} title="Suport" />
+          <NavItem icon={Cog} title="Settings" />
+        </nav>
+
+        <UsedSpaceWidget />
+      </div>
     </aside>
   )
 }
