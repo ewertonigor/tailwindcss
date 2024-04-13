bc2769947d5385593852d013ff5991c002d850f6
Ewerton Igor <ewertonigor@icloud.com>
2024-04-13 20:36:40 -0300

    Menu de Navegação - SideBar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,19 +1,21 @@
 import { Blend, Search } from 'lucide-react'
+import MainNavigation from './MainNavigation'
 
 export default function SideBar() {
   return (
     <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
-      <strong className="flex items-center gap-2 text-xl font-semibold text-zinc-900">
+      <strong className="mx-1 flex items-center gap-2 text-xl font-semibold text-zinc-900">
         <Blend />
         <span>Untitled UI</span>
       </strong>
       <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <Search className="h-5 w-5 text-zinc-500" />
         <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+          className="mx-1 flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
           placeholder="Search"
         />
       </div>
+      <MainNavigation />
     </aside>
   )
 }
