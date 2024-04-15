24c76430bfc6396b761b228fd918bb002fb786d9
Ewerton Igor <ewertonigor@icloud.com>
2024-04-15 19:58:45 -0300

    Upload do Avatar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,17 @@ const config: Config = {
         profile: 'max-content 1fr min-content',
         form: 'minmax(7.5rem, 17.5reem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+
+      borderWidth: {
+        6: '6px',
+      },
+
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+      },
+
       maxWidth: {
         app: '700px',
       },
