4118d5519cd3d7464dd4bd8de8c9b9d39560ddd6
Ewerton Igor <ewertonigor@icloud.com>
2024-04-20 11:37:32 -0300

    Responsividade da Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -16,7 +16,7 @@ import InputRoot, { InputControl, InputPrefix } from '../Input'
 
 export default function SideBar() {
   return (
-    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="lg: fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:relative lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 ">
       <strong className="mx-1 flex items-center gap-2 text-xl font-semibold text-zinc-900">
         <Blend />
         <span>Untitled UI</span>
