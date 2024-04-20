77b01b273f77dd36ddb6d3f339175e6f23d02770
Ewerton Igor <ewertonigor@icloud.com>
2024-04-20 17:09:01 -0300

    Tema dark do formulário

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -15,7 +15,7 @@ export default function TabItem({
   return (
     <TabsTrigger
       value={value}
-      className="data-[state=active]: group relative px-1 pb-4 text-sm text-zinc-500 outline-none hover:text-violet-700"
+      className="data-[state=active]: group relative px-1 pb-4 text-sm text-zinc-500 outline-none hover:text-violet-700 dark:text-zinc-400 dark:hover:text-violet-300 dark:data-[state=active]:text-violet-300"
     >
       <span className="group-focus-visible::ring-2 group-focus-visible::ring-offset-4 whitespace-nowrap rounded group-focus:ring-violet-400">
         {title}
@@ -24,7 +24,7 @@ export default function TabItem({
       {isSelected && (
         <motion.div
           layoutId="activeTab"
-          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700 dark:bg-violet-300"
         />
       )}
     </TabsTrigger>
